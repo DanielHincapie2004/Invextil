d815007c95667fbba6de836fd5cdd566a89cf4de
Daniel Hincapié <54695775+DanielHincapie2004@users.noreply.github.com>
2023-07-05 22:56:31 -0500

    Add files via upload

---
+++ InvextilBD.sql
@@ -0,0 +1,291 @@
+USE [Invextil]
+GO
+/****** Object:  Table [dbo].[Cita]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cita](
+	[idCita] [int] IDENTITY(1,1) NOT NULL,
+	[idEmpleado] [int] NULL,
+	[cliente] [varchar](150) NULL,
+	[fecha] [varchar](100) NULL,
+	[hora] [varchar](100) NULL,
+	[direccion] [varchar](150) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idCita] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Configuracion]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Configuracion](
+	[recurso] [varchar](50) NULL,
+	[propiedad] [varchar](50) NULL,
+	[valor] [varchar](60) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[DetalleVenta]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DetalleVenta](
+	[idDetalleVenta] [int] IDENTITY(1,1) NOT NULL,
+	[idVenta] [int] NULL,
+	[idProducto] [int] NULL,
+	[marcaProducto] [varchar](100) NULL,
+	[descripcionProducto] [varchar](100) NULL,
+	[categoriaProducto] [varchar](100) NULL,
+	[cantidad] [int] NULL,
+	[precio] [decimal](10, 2) NULL,
+	[total] [decimal](10, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idDetalleVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Empleado]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Empleado](
+	[idEmpleado] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[correo] [varchar](50) NULL,
+	[idRol] [int] NULL,
+	[urlFoto] [varchar](500) NULL,
+	[nombreFoto] [varchar](100) NULL,
+	[clave] [varchar](100) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idEmpleado] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Familia]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Familia](
+	[idCategoria] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](50) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idCategoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Menu]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Menu](
+	[idMenu] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](30) NULL,
+	[idMenuPadre] [int] NULL,
+	[icono] [varchar](30) NULL,
+	[controlador] [varchar](30) NULL,
+	[paginaAccion] [varchar](30) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idMenu] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Muestra]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Muestra](
+	[idMuestra] [int] IDENTITY(1,1) NOT NULL,
+	[idTela] [int] NULL,
+	[documentoCliente] [varchar](20) NULL,
+	[nombreCliente] [varchar](100) NULL,
+	[direccion] [varchar](100) NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idMuestra] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[NumeroCorrelativo]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NumeroCorrelativo](
+	[idNumeroCorrelativo] [int] IDENTITY(1,1) NOT NULL,
+	[ultimoNumero] [int] NULL,
+	[cantidadDigitos] [int] NULL,
+	[gestion] [varchar](100) NULL,
+	[fechaActualizacion] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idNumeroCorrelativo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rol]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rol](
+	[idRol] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](30) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idRol] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[RolMenu]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RolMenu](
+	[idRolMenu] [int] IDENTITY(1,1) NOT NULL,
+	[idRol] [int] NULL,
+	[idMenu] [int] NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idRolMenu] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tela]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tela](
+	[idProducto] [int] IDENTITY(1,1) NOT NULL,
+	[codigoBarra] [varchar](50) NULL,
+	[marca] [varchar](50) NULL,
+	[descripcion] [varchar](100) NULL,
+	[idCategoria] [int] NULL,
+	[stock] [int] NULL,
+	[urlImagen] [varchar](500) NULL,
+	[nombreImagen] [varchar](100) NULL,
+	[precio] [decimal](10, 2) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TipoDocumentoVenta]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TipoDocumentoVenta](
+	[idTipoDocumentoVenta] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](50) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idTipoDocumentoVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Venta]    Script Date: 05/07/2023 22:52:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Venta](
+	[idVenta] [int] IDENTITY(1,1) NOT NULL,
+	[numeroVenta] [varchar](6) NULL,
+	[idTipoDocumentoVenta] [int] NULL,
+	[idEmpleado] [int] NULL,
+	[documentoCliente] [varchar](10) NULL,
+	[nombreCliente] [varchar](20) NULL,
+	[subTotal] [decimal](10, 2) NULL,
+	[impuestoTotal] [decimal](10, 2) NULL,
+	[Total] [decimal](10, 2) NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Empleado] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Familia] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Menu] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Muestra] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Rol] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[RolMenu] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Tela] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[TipoDocumentoVenta] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Venta] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Cita]  WITH CHECK ADD FOREIGN KEY([idEmpleado])
+REFERENCES [dbo].[Empleado] ([idEmpleado])
+GO
+ALTER TABLE [dbo].[DetalleVenta]  WITH CHECK ADD FOREIGN KEY([idVenta])
+REFERENCES [dbo].[Venta] ([idVenta])
+GO
+ALTER TABLE [dbo].[DetalleVenta]  WITH CHECK ADD  CONSTRAINT [FK_DetalleVenta_Tela] FOREIGN KEY([idProducto])
+REFERENCES [dbo].[Tela] ([idProducto])
+GO
+ALTER TABLE [dbo].[DetalleVenta] CHECK CONSTRAINT [FK_DetalleVenta_Tela]
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD FOREIGN KEY([idRol])
+REFERENCES [dbo].[Rol] ([idRol])
+GO
+ALTER TABLE [dbo].[Menu]  WITH CHECK ADD FOREIGN KEY([idMenuPadre])
+REFERENCES [dbo].[Menu] ([idMenu])
+GO
+ALTER TABLE [dbo].[Muestra]  WITH CHECK ADD FOREIGN KEY([idTela])
+REFERENCES [dbo].[Tela] ([idProducto])
+GO
+ALTER TABLE [dbo].[RolMenu]  WITH CHECK ADD FOREIGN KEY([idMenu])
+REFERENCES [dbo].[Menu] ([idMenu])
+GO
+ALTER TABLE [dbo].[RolMenu]  WITH CHECK ADD FOREIGN KEY([idRol])
+REFERENCES [dbo].[Rol] ([idRol])
+GO
+ALTER TABLE [dbo].[Tela]  WITH CHECK ADD FOREIGN KEY([idCategoria])
+REFERENCES [dbo].[Familia] ([idCategoria])
+GO
+ALTER TABLE [dbo].[Venta]  WITH CHECK ADD FOREIGN KEY([idTipoDocumentoVenta])
+REFERENCES [dbo].[TipoDocumentoVenta] ([idTipoDocumentoVenta])
+GO
+ALTER TABLE [dbo].[Venta]  WITH CHECK ADD FOREIGN KEY([idEmpleado])
+REFERENCES [dbo].[Empleado] ([idEmpleado])
+GO
